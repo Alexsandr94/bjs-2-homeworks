af8b485e47e084880435167f9ff84c8c4cc53913
Alexsandr94 <lisan940@gmail.com>
2025-11-27 23:18:09 +0500

    Fix errors

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -27,26 +27,22 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   contribution = parseFloat(contribution);
   amount = parseFloat(amount);
   countMonths = parseFloat(countMonths);
-
+  
   if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
     return false;
   }
-
+  
   const monthlyPercent = percent / 100 / 12;
-
   const loanBody = amount - contribution;
-
-  const monthlyPayment = loanBody *
-    (monthlyPercent + monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
-
+  
+  if (loanBody <= 0) {
+    return contribution;
+  }
+  
+  const monthlyPayment = loanBody * 
+    (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
+  
   const totalAmount = monthlyPayment * countMonths + contribution;
-
+  
   return Number(totalAmount.toFixed(2));
 }
-
-console.log(calculateTotalMortgage(10, 0, 50000, 12)); // 52749.53
-console.log(calculateTotalMortgage(10, 1000, 50000, 12)); // 51694.54
-console.log(calculateTotalMortgage(10, 0, 20000, 24)); // 22149.56
-console.log(calculateTotalMortgage(10, 1000, 20000, 24)); // 21042.09
-console.log(calculateTotalMortgage(10, 20000, 20000, 24)); // 20000 (только первоначальный взнос)
-console.log(calculateTotalMortgage(10, 0, 10000, 36)); // 11616.19
