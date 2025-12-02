9cc40bc0b6e71694f8491ae9000920f60d0aadbb
Alexsandr94 <lisan940@gmail.com>
2025-12-02 22:15:44 +0500

    Student

--- 5.classes/student_tests.js
+++ 5.classes/student_tests.js
@@ -1,59 +1,70 @@
-describe('Домашнее задание к лекции 5 «Классы». Дополнительное задание', () => {
-  describe('Задача №3', () => {
-    let student;
-  
-    beforeEach(function(){
-      student = new Student("Иван Петров");
-    });
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
 
-    it('создание объекта Student', () => {
-      expect(student).toBeDefined();
-      expect(student.name).toEqual("Иван Петров");
-      expect(student.marks).toEqual({});
-    });
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      console.log(`Оценка ${mark} по предмету ${subject} не добавлена: должна быть от 2 до 5`);
+      return;
+    }
 
-    it('добавление оценок по разным предметам', () => {
-      student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(5, "математика");
-      expect(student.marks).toEqual({"математика": [3, 5]});
-      student.addMark(5, "физика");
-      expect(student.marks).toEqual({"математика": [3, 5], "физика": [5]});
-    });
+    if (!this.marks.hasOwnProperty(subject)) {
+      this.marks[subject] = [];
+    }
 
-    it('невозможность добавления некорректных оценок', () => {
-      student.addMark(0, "математика");
-      expect(student.marks).toEqual({});
-      student.addMark(3, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(10, "математика");
-      expect(student.marks).toEqual({"математика": [3]});
-      student.addMark(7, "физика");
-      expect(student.marks).toEqual({"математика": [3]});
-    });
+    this.marks[subject].push(mark);
+  }
 
-    it('подсчёт средней оценки по предмету', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      expect(student.getAverageBySubject("математика")).toEqual(4);
-    });
+  getAverageBySubject(subject) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) {
+      return 0;
+    }
 
-    it('подсчёт средней оценки по несуществующему предмету', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      expect(student.getAverageBySubject("программирование")).toEqual(0);
-    });
+    const sum = this.marks[subject].reduce((total, mark) => total + mark, 0);
+    return sum / this.marks[subject].length;
+  }
 
-    it('подсчёт общей средней оценки пустого объекта оценок', () => {
-      expect(student.getAverage()).toEqual(0);
-    });
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    
+    if (subjects.length === 0) {
+      return 0;
+    }
 
-    it('подсчёт общей средней оценки', () => {
-      student.addMark(3, "математика");
-      student.addMark(5, "математика");
-      student.addMark(5, "история");
-      student.addMark(5, "история");
-      expect(student.getAverage()).toEqual(4.5);
-    });
-  });
-});
+    const totalAverage = subjects.reduce((sum, subject) => {
+      return sum + this.getAverageBySubject(subject);
+    }, 0);
+
+    return totalAverage / subjects.length;
+  }
+}
+
+const student = new Student("Олег Никифоров");
+
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); 
+
+console.log(student.getAverageBySubject("физика")); 
+console.log(student.getAverageBySubject("биология")); 
+console.log(student.getAverage()); 
+console.log(student.marks); 
+
+const student2 = new Student("Анна Петрова");
+console.log(student2.getAverage()); 
+
+student2.addMark(4, "математика");
+student2.addMark(5, "математика");
+student2.addMark(3, "математика");
+student2.addMark(5, "литература");
+student2.addMark(4, "литература");
+student2.addMark(1, "литература"); 
+
+console.log(student2.getAverageBySubject("математика")); 
+console.log(student2.getAverageBySubject("литература")); 
+console.log(student2.getAverageBySubject("история")); 
+console.log(student2.getAverage()); 
